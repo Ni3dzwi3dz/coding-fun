04fcf77516ff37257187872deaaf5456a3bbca73
Ni3dzwi3dz <niedzwiedz.sikorski@gmail.com>
2025-06-03 21:04:16 +0200

    added script

---
+++ utils/kata.py
@@ -0,0 +1,42 @@
+from typer import Typer, Argument, Option
+from utils.utils.kata import get_kata_dict
+
+
+app = Typer(
+    name="kata",
+    help="Manage CodeWars katas")
+
+
+
+@app.command()
+def copy(
+    kata_id: str = Argument(..., help="The ID of the kata to copy"),
+):
+    kata_info = get_kata_dict(kata_id)
+
+    title = kata_info.get("name","Unknown Kata")
+    rank = kata_info.get("rank", 8)
+    description = kata_info.get("description", "No description available.")
+    languages = kata_info.get("languages", [])
+    url = kata_info.get("url", "https://www.codewars.com/kata/" + kata_id)
+
+
+    kata_dir = get_kata_path(title, rank)
+
+    if not kata_dir.exists():
+        kata_dir.mkdir(parents=True, exist_ok=True)
+
+    readme_path = kata_dir / "README.md"
+
+    jinja_env = Environment(
+        loader=FileSystemLoader("templates")
+        )
+    
+    template = jinja_env.get_template("kata_readme.template")
+
+    template.render(
+        title=title,
+        description=description,
+        languages=languages,
+        link=url
+    )
---
+++ utils/utils/kata.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import httpx
+
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://www.codewars.com/api/v1/code-challenges/"
+
+
+def get_kata_url(kata_id: str) -> str:
+    """
+    Constructs the URL for a specific CodeWars kata.
+
+    Args:
+        kata_id (str): The ID of the kata.
+
+    Returns:
+        str: The full URL to the kata.
+    """
+    return f"{BASE_URL}{kata_id}"
+
+def get_kata_dict(kata_id: str) -> dict:
+    '''
+    Fetches the HTML content of a specific CodeWars kata.
+    Args:
+        kata_id (str): The ID of the kata.'''
+    url = get_kata_url(kata_id)
+    response = httpx.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch kata {kata_id}: {response.status_code} {response.reason_phrase}")
+
+def get_kata_path(name: str, rank: int) -> Path:
+    return Path(f"codewars/{abs(rank)}kyu/{name.replace(' ', '_').replace('-','_').lower()}")
